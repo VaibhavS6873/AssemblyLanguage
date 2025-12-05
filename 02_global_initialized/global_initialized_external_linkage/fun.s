225b1a6baf7a1a9374bf5e78ba26d84e76ac0839
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2025-12-05 13:05:14 +0530

    Create fun.s

---
+++ 02_global_initialized/global_initialized_external_linkage/fun.s
@@ -0,0 +1,28 @@
+.section .rodata
+ 	msg_fun_print:
+ 	.string "In fun:%d\n"
+ 	
+ 	
+ .section .data
+ 	.globl g_iNo
+ 	.type g_iNo, @object
+ 	.size g_iNo,4
+ 	.align 4
+ g_iNo:
+ 	.int 10
+ 	
+ .section .data
+ .globl fun
+ .type fun, @function
+ fun:
+ 	pushl %ebp
+ 	movl %esp,%ebp
+ 	
+ 	pushl g_iNo
+ 	pushl $msg_fun_print
+ 	call printf
+ 	addl $8,%esp
+ 	
+ 	movl %ebp,%esp
+ 	popl %ebp
+ 	ret
