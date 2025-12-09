e52546052374598e4dc639603e6d24faa96e8f12
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2025-12-09 12:55:16 +0530

    Create main.s

---
+++ 03_global_unintialized/global_uninitialzed_external_linkage_error/main.s
@@ -0,0 +1,26 @@
+.section .rodata
+	msg_main_print:
+	.string "In main:%d\n"
+
+.section .bss
+ 	.comm g_iNo, 4, 4
+ 	
+.section .text
+.globl main
+.type main, @function
+main:
+	pushl %ebp
+	mov %esp, %ebp
+	
+	pushl g_iNo
+	pushl $msg_main_print 
+	call printf
+	addl $8, %esp
+	
+	call fun
+	
+	pushl $0
+	call exit
+	
+	
+	
