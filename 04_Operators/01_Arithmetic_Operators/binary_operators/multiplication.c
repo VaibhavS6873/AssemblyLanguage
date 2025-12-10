ed116bccee7148ac55f0c5e47160c957db38fe07
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2025-12-10 15:51:09 +0530

    Create multiplication.c

---
+++ 04_Operators/01_Arithmetic_Operators/binary_operators/multiplication.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int iNo1;
+	int iNo2;
+	int iAns;
+	
+	printf("Enter operand 1:\t");
+	scanf("%d",&iNo1);
+	
+	printf("Enter operand 2:\t");
+	scanf("%d",&iNo2);
+	
+	iAns = iNo1 * iNo2;
+	
+	printf("Multiplication is = %d", iAns);
+	exit(0);
+}
