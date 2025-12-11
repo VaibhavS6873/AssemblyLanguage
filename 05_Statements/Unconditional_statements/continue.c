4b122c1916f483ae20b328d1782c0549d13dca06
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2025-12-11 12:18:39 +0530

    Create continue.c

---
+++ 05_Statements/Unconditional_statements/continue.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int iNo;
+	int iCounter;
+	
+	printf("Enter the number:\t");
+	scanf("%d",&iNo);
+	
+	for(iCounter = 1; iCounter < iNo; iCounter++)
+	{
+		if(iCounter % 2 == 0)
+			continue;
+		printf("%d\n", iCounter);
+	}
+	exit(0);
+}
