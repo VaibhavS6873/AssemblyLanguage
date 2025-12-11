d2299368fbe9a30c0bcad8fec3abe6205e6db29a
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2025-12-11 12:18:53 +0530

    Create continue.s

---
+++ 05_Statements/Unconditional_statements/continue.s
@@ -0,0 +1,60 @@
+.section .rodata
+	msg_main_print1:
+	.string	"Enter the number:\t"
+	
+	msg_main_scan:
+	.string	"%d"
+	
+	msg_main_print2:
+	.string	"%d\n"
+	
+	
+.section .text
+.globl	main
+.type main, @function
+main:
+	pushl	%ebp
+	movl	%esp,%ebp
+	
+	subl	$8,%esp
+	
+	pushl	$msg_main_print1
+	call	printf
+	addl	$4,%esp
+	
+	leal	-4(%ebp),%ebx
+	pushl	%ebx
+	pushl	$msg_main_scan
+	call	scanf
+	addl	$8,%esp
+	
+	movl	$1, -8(%ebp)
+	jmp		label_for_condition
+	
+label_for:
+	xorl	%edx,%edx
+	movl	$2,%ecx
+	divl	%ecx
+	cmpl	$0,%edx
+	jne		label_below
+	jmp		label_add
+	
+label_below:
+	pushl	-8(%ebp)
+	pushl	$msg_main_print2
+	call	printf
+	addl	$8,%esp
+
+label_add:
+	addl	$1, -8(%ebp)
+	
+label_for_condition:
+	movl	-8(%ebp),%eax
+	movl	-4(%ebp),%edx
+	cmpl	%edx, %eax
+	jl		label_for
+
+label_out:	
+	pushl	$0
+	call	exit
+	
