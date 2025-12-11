e526cca73d4010b045be377e141f03cb93f64164
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2025-12-11 11:41:36 +0530

    Create 4_if_else_of_switch.s

---
+++ 05_Statements/Conditional_statments/4_if_else_of_switch.s
@@ -0,0 +1,88 @@
+.section .rodata
+	msg_main_print1:
+	.string "1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n"
+	
+	msg_main_print2:
+	.string "Enter your choice:\t"
+	
+	msg_main_print3:
+	.string "Addition\n"
+	
+	msg_main_print4:
+	.string "Subtraction\n"
+	
+	msg_main_print5:
+	.string "Multiplication\n"
+	
+	msg_main_print6:
+	.string "Division\n"
+	
+	msg_main_print7:
+	.string "Invalid choice\n"
+	
+	msg_main_scan:
+	.string	"%d"
+	
+.section .text
+.globl main
+.type main, @function
+main:
+	pushl	%ebp
+	movl	%esp, %ebp
+	
+	subl	$4, %esp
+	
+	pushl	$msg_main_print1
+	call	printf
+	addl	$4, %esp
+	
+	pushl	$msg_main_print2
+	call	printf
+	addl	$4, %esp
+	
+	leal	-4(%ebp), %ebx
+	pushl	%ebx
+	pushl	$msg_main_scan
+	call	scanf
+	addl	$8, %esp
+	
+	movl	-4(%ebp),%eax
+	cmpl	$1,%eax
+	jne	label_else_if_1
+	pushl	$msg_main_print3
+	call	printf
+	addl	$4, %esp
+	jmp		label_out
+	
+label_else_if_1:
+	cmpl	$2,	%eax
+	jne		label_else_if_2
+	pushl	$msg_main_print4
+	call	printf
+	addl	$4, %esp
+	jmp		label_out
+
+label_else_if_2:
+	cmpl	$3,	%eax
+	jne		label_else_if_3
+	pushl	$msg_main_print5
+	call	printf
+	addl	$4, %esp
+	jmp		label_out
+	
+label_else_if_3:
+	cmpl	$4,	%eax
+	jne		label_else
+	pushl	$msg_main_print6
+	call	printf
+	addl	$4, %esp
+	jmp		label_out
+	
+label_else:
+	pushl	$msg_main_print7
+	call	printf
+	addl	$4, %esp
+	
+label_out:
+	pushl	$0
+	call	exit
