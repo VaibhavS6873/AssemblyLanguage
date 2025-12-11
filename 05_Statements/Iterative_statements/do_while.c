3d4b1bf93ccfcb85e9c850181fb6115380f32710
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2025-12-11 12:15:48 +0530

    Create do_while.c

---
+++ 05_Statements/Iterative_statements/do_while.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int iNo;
+	int iCounter;
+	
+	printf("Enter the number:\t");
+	scanf("%d",&iNo);
+	
+	iCounter = 0;
+	do
+	{
+		printf("%d\n",iCounter);
+		iCounter++;
+	} 
+	exit(0);
+}
