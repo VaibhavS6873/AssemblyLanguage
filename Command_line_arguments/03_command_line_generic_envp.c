2c8429631d396b0b64a94d5cdc91dff76f91e295
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2025-12-21 20:10:45 +0530

    Create 03_command_line_generic_envp.c

---
+++ Command_line_arguments/03_command_line_generic_envp.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(int argc, char* argv[], char* envp[])
+{
+	int iCounter;
+	int iSum = 0;
+	
+	printf("argc is %d\n\n",argc);
+	
+	if(argc != 3)
+	{
+		puts("Invalid arguments:prog_name arg1 arg2");
+		
+		goto label;
+	}
+	
+	puts("values are:");
+	for(iCounter  = 0; iCounter < argc; iCounter++)
+		puts(argv[iCounter]);
+	printf("\n");
+	
+	iSum = atoi(argv[1]) + atoi(argv[2]);
+	
+label:
+	puts("Envp values are:");
+	for(iCounter = 0; envp[iCounter]!=NULL; iCounter++)
+		puts(envp[iCounter]);
+		
+	printf("\nsum is %d\n", iSum);
+	
+	exit(0);
+}
