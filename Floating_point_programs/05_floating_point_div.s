fe9a633a88491031766cb9163b4b8bddff0a658e
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2025-12-21 20:33:46 +0530

    Create 05_floating_point_div.s

---
+++ Floating_point_programs/05_floating_point_div.s
@@ -0,0 +1,88 @@
+.section .rodata
+	msg_main_print1:
+	.string "fNo1 = %f\tfNo2 = %f\n"
+	
+	msg_main_print2:
+	.string	"fAns1 = %f\t\n"
+	
+	msg_main_print3:
+	.string	"fAns2 = %f\t\n"
+	
+	msg_main_print4:
+	.string	"fAns3 = %f\t\n"
+	
+	int_value:
+	.int 2
+	
+.section .data
+	.globl	fNo1
+	.type	fNo1,@object
+	.size	fNo1,4
+	.align	4
+fNo1:
+	.float	10.22
+	
+	.globl	fNo2
+	.type	fNo2,@object
+	.size	fNo2,4
+	.align	4
+fNo2:
+	.float	20.22
+	
+
+
+.section .bss
+	.comm	fAns1,4,4
+	.comm	fAns2,4,4
+	.comm	fAns3,4,4
+
+
+.section .text
+.globl	main
+.type	main,@function
+main:
+		andl	$-16,%esp
+		pushl	%ebp
+		movl	%esp,%ebp
+		
+		subl	$32,%esp
+		
+		movl	$msg_main_print1,(%esp)
+		flds	fNo2
+		flds	fNo1
+		fstpl	4(%esp)
+		fstpl	12(%esp)
+		call	printf	
+		
+		flds	fNo1 	#st(1)
+		flds	fNo2	#st(0)
+		fdiv	%st(1),%st(0)
+		fstps	fAns1
+		
+		flds	fNo2	#st(0)
+		fdivr	%st(1),%st(0)
+		fstps	fAns2
+		
+		fidivs	int_value
+		fsts	fAns3
+		
+		movl	$msg_main_print2,(%esp)
+		flds	fAns1
+		fstpl	4(%esp)
+		call	printf
+		
+		movl	$msg_main_print3,(%esp)
+		flds	fAns2
+		fstpl	4(%esp)
+		call	printf
+		
+		movl	$msg_main_print4,(%esp)
+		flds	fAns3
+		fstpl	4(%esp)
+		call	printf
+		
+		movl	$0,(%esp)
+		call	exit
+		
+		
+			
