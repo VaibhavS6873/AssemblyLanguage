cf42cdfa475626a0df70eccdfb2c7288c2bf3b45
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2025-12-21 20:09:52 +0530

    Create 02_command_line_envp.c

---
+++ Command_line_arguments/02_command_line_envp.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+	int iCounter;
+	
+	if(argc != 1)
+	{
+		puts("Invalid arguments:prog_name");
+		exit(-1);
+	}
+	
+	puts("Envp values are:");
+	for(iCounter = 0; envp[iCounter]!=NULL; iCounter++)
+		puts(envp[iCounter]);
+		
+	exit(0);
+}
