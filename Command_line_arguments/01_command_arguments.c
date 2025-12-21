ac0f85cdc4b5149ddb816d58b2fea04f560438aa
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2025-12-21 20:08:42 +0530

    Create 01_command_arguments.c

---
+++ Command_line_arguments/01_command_arguments.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	int iCounter;
+	int iSum;
+	
+	printf("argc is %d\n\n", argc);
+	
+	if(argc != 3)
+	{
+		puts("Invalid arguments:prog_name no1 no2");
+		exit(-1);
+	}
+	
+	puts("Values are:");
+	for(iCounter = 0; iCounter < argc; iCounter++)
+		puts(argv[iCounter]);
+	printf("\n");
+	
+	iSum = atoi(argv[1]) + atoi(argv[2]);
+	
+	printf("Sum is %d\n", iSum);
+	exit(0);
+}
