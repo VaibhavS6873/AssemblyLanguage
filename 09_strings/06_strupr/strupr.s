dadd0330c0b5c80c4a614f69b40bfe9c32e09e56
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2025-12-21 19:27:27 +0530

    Create strupr.s

---
+++ 09_strings/06_strupr/strupr.s
@@ -0,0 +1,38 @@
+.section .rodata
+	msg_main_print1:
+	.string	"\n\nEnter The String:\t"
+	
+	msg_main_print2:
+	.string	"\nString In Upper Case Is:\t"
+	
+	
+.section .text
+.globl	main
+.type	main, @function
+main:
+		andl	$-16,%esp
+		pushl	%ebp
+		movl	%esp,%ebp
+		
+		subl	$32,%esp
+		
+		movl	$msg_main_print1,(%esp)
+		call	printf
+		
+		leal	-20(%ebp),%eax
+		movl	%eax,(%esp)
+		call	gets
+		
+		leal	-20(%ebp),%eax
+		movl	%eax,(%esp)
+		call	strupr
+
+		movl	$msg_main_print2,(%esp)
+		call	printf
+		
+		leal	-20(%ebp),%eax
+		movl	%eax,(%esp)
+		call	puts
+		
+		movl	$0,(%esp)
+		call	exit
