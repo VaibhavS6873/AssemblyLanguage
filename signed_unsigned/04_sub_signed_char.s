086c46045b30ebf5a0924b7f79d40f01c857460b
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2025-12-22 12:34:43 +0530

    Create 04_sub_signed_char.s

---
+++ signed_unsigned/04_sub_signed_char.s
@@ -0,0 +1,34 @@
+.section .rodata
+	msg_main_print1:
+	.string "overflow bit on\t"
+	
+	msg_main_print2:
+	.string "overflow bit off\t"
+	
+	
+.section .text
+.globl	main
+.type	main,@function
+main:
+		addl	$-16,%esp
+		
+		subl	$16,%esp
+		
+		movb	$-127,%al
+		movb	$2,%dl
+		
+		subb	%dl,%al
+		
+		jo		label_carry_on
+		movl	$msg_main_print2,(%esp)
+		call	printf
+		jmp		label_out
+		
+	label_carry_on:
+		movl	$msg_main_print1,(%esp)
+		call	printf
+		
+		
+	label_out:
+		movl	$0,(%esp)
+		call	exit
